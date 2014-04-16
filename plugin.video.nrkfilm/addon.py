f25dc8f8b3d98b7b9ad34ced5d620b239e1386ad
Petter H. Juliussen <post@petterhj.net>
2014-04-16 17:04:19 +0000

    cleanup

--- plugin.video.nrkfilm/addon.py
+++ plugin.video.nrkfilm/addon.py
@@ -11,44 +11,44 @@ plugin = Plugin()
 # Films
 @plugin.route('/')
 def index():
-	# Content type
-	plugin.set_content('movies')
+    # Content type
+    plugin.set_content('movies')
 
-	# Get diary
-	cache = plugin.storage_path + 'cache'
+    # Get diary
+    cache = plugin.storage_path + 'cache'
 
-	films = nrkfilm.NRKFilm(cache).feature_films()
+    films = nrkfilm.NRKFilm(cache).feature_films()
 
-	print films
-	
-	items = [{
-		'icon':			film['tmdb']['poster'] or film['nrk']['poster'],
-		'thumbnail': 	film['tmdb']['poster'] or film['nrk']['poster'],
-		'label':		film['tmdb']['original_title'] or film['nrk']['original_title'] or film['nrk']['title'],
-		'info': {
-			'title':		film['tmdb']['title'] or nrk['nrk']['title']
-			'originaltitle':film['tmdb']['original_title'] or nrk['nrk']['original_title'] or film['tmdb']['title'] or nrk['nrk']['title'],
-			'year':			film['tmdb']['year'] or film['nrk']['year'],
-			'genre': 		', '.join(film['tmdb']['genre']),
-			'rating': 		10
-		},
-		'path':			film['nrk']['stream'] if not _isDebug() else plugin.url_for('index')
-	} for film in films]
-		
-	
-	# Return
-	return items
+    print films
+    
+    items = [{
+        'icon':         film['tmdb']['poster'] or film['nrk']['poster'],
+        'thumbnail':    film['tmdb']['poster'] or film['nrk']['poster'],
+        'label':        film['tmdb']['original_title'] or film['nrk']['original_title'] or film['nrk']['title'],
+        'info': {
+            'title':        film['tmdb']['title'] or nrk['nrk']['title']
+            'originaltitle':film['tmdb']['original_title'] or nrk['nrk']['original_title'] or film['tmdb']['title'] or nrk['nrk']['title'],
+            'year':         film['tmdb']['year'] or film['nrk']['year'],
+            'genre':        ', '.join(film['tmdb']['genre']),
+            'rating':       10
+        },
+        'path':         film['nrk']['stream'] if not _isDebug() else plugin.url_for('index')
+    } for film in films]
+        
+    
+    # Return
+    return items
 
 
 # Debug
 def _isDebug():
-	try:
-		import xbmc
-	except ImportError:
-		return True
-	else:
-		return False
+    try:
+        import xbmc
+    except ImportError:
+        return True
+    else:
+        return False
 
 # Main
 if __name__ == '__main__':
-	plugin.run()
+    plugin.run()
