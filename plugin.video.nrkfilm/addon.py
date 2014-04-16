b3dd413d8a04630f48cf2187607338dd3ca36fce
Petter H. Juliussen <post@petterhj.net>
2014-04-16 20:42:20 +0000

    update

--- plugin.video.nrkfilm/addon.py
+++ plugin.video.nrkfilm/addon.py
@@ -39,14 +39,15 @@ def index():
     items = []
 
     for film in films:
+        print film
         item = {
-            'icon':         film['tmdb']['poster'] if film['tmdb'] else film['nrk']['poster'],
-            'thumbnail':    film['tmdb']['poster'] if film['tmdb'] else film['nrk']['poster'],
-            'label':        film['tmdb']['original_title'] if film['tmdb'] else film['nrk']['original_title'] or film['nrk']['title'],
+            'icon':         film['tmdb']['poster'] if film['tmdb'] else film['nrk']['fanart'],
+            'thumbnail':    film['tmdb']['poster'] if film['tmdb'] else film['nrk']['fanart'],
+            'label':        film['tmdb']['original_title'] if film['tmdb'] else film['nrk']['original_title'] if film['nrk']['original_title'] else film['nrk']['title'],
             'info': {
                 'title':        film['tmdb']['title'] if film['tmdb'] else film['nrk']['title'],
-                'originaltitle':film['tmdb']['original_title'] if film['tmdb'] else film['nrk']['original_title'] or film['tmdb']['title'] or film['nrk']['title'],
-                'year':         film['tmdb']['year'] if film['tmdb'] else film['nrk']['year'],
+                'originaltitle':film['tmdb']['original_title'] or film['tmdb']['title'] if film['tmdb'] else film['nrk']['original_title'] if film['nrk']['original_title'] else film['nrk']['title'],
+                'year':         film['tmdb']['year'] if film['tmdb'] else film['nrk']['year'] if film['nrk']['year'] else '',
                 'genre':        ', '.join(film['tmdb']['genre']) if film['tmdb'] else '',
                 'rating':       10
             },
