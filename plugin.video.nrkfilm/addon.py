48d77191f4f04ec4ca0344796fddb1c7c37f3028
Petter H. Juliussen <post@petterhj.net>
2014-04-17 13:40:09 +0000

    added stream info

--- plugin.video.nrkfilm/addon.py
+++ plugin.video.nrkfilm/addon.py
@@ -24,27 +24,41 @@ def index():
     films = nrkfilm.NRKFilm(cache).feature_films()
     
     # Items
-    items = [{
-        'icon':         film['tmdb']['poster'] or film['nrk']['poster'] or film['nrk']['fanart'] or '',
-        'thumbnail':    film['tmdb']['poster'] or film['nrk']['poster'] or film['nrk']['fanart'] or '',
-        'label':        film['tmdb']['title'] or film['nrk']['title'] or '',
-        'info': {
-            'title':        film['tmdb']['title'] or film['nrk']['title'] or '',
-            'originaltitle':film['tmdb']['original_title'] or film['nrk']['original_title'] or film['tmdb']['title'] or film['nrk']['title'] or '',
-            'plot':         film['tmdb']['description'] or film['nrk']['description'] or '',
-            'plotoutline':  film['tmdb']['description'] or film['nrk']['description'] or '',
-            'year':         film['tmdb']['year'] or film['nrk']['year'] or 0,
-            'genre':        ', '.join(film['tmdb']['genre']),
-            'director':     film['tmdb']['director'][0] if len(film['tmdb']['director']) > 0 else '',
-            'writer':       film['tmdb']['writer'][0] if len(film['tmdb']['writer']) > 0 else '',
-            'cast':         film['tmdb']['cast'],
-            'rating':       10
-        },
-        'properties': {
-            'fanart_image': film['tmdb']['fanart'] or film['nrk']['fanart'] or '',
-        },
-        'path':         film['nrk']['stream'] if not _isDebug() else plugin.url_for('index')
-    } for film in films]
+    items = [
+        {
+            'icon':         film['tmdb']['poster'] or film['nrk']['poster'] or film['nrk']['fanart'] or '',
+            'thumbnail':    film['tmdb']['poster'] or film['nrk']['poster'] or film['nrk']['fanart'] or '',
+            'label':        film['tmdb']['title'] or film['nrk']['title'] or '',
+            'info': {
+                'title':        film['tmdb']['title'] or film['nrk']['title'] or '',
+                'originaltitle':film['tmdb']['original_title'] or film['nrk']['original_title'] or film['tmdb']['title'] or film['nrk']['title'] or '',
+                'plot':         film['tmdb']['description'] or film['nrk']['description'] or '',
+                'plotoutline':  film['tmdb']['description'] or film['nrk']['description'] or '',
+                'year':         film['tmdb']['year'] or film['nrk']['year'] or 0,
+                'genre':        ', '.join(film['tmdb']['genre']),
+                'director':     film['tmdb']['director'][0] if len(film['tmdb']['director']) > 0 else '',
+                'writer':       film['tmdb']['writer'][0] if len(film['tmdb']['writer']) > 0 else '',
+                'cast':         film['tmdb']['cast'],
+                'rating':       10
+            },
+            'properties': {
+                'fanart_image': film['tmdb']['fanart'] or film['nrk']['fanart'] or '',
+            },
+            'path':         film['nrk']['stream'] if not _isDebug() else plugin.url_for('index'),
+            'is_playable':  True,
+            'stream_info': {
+                'video': {
+                    'codec':        'h264',
+                    'width':        1280,
+                    'height':       720
+                },
+                'audio': {
+                    'codec':        'aac',
+                    'channels':     2
+                }
+            }
+        }
+    for film in films]
     
     # Return
     return items
