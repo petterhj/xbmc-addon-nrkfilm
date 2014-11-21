e561433598e676e2e3426d2d66fc46a6a9a57335
Petter Holt Juliussen <post@petterhj.net>
2014-11-21 17:57:07 -0500

    changed logging output

--- plugin.video.nrkfilm/resources/lib/nrkfilm/nrkfilm.py
+++ plugin.video.nrkfilm/resources/lib/nrkfilm/nrkfilm.py
@@ -239,7 +239,7 @@ class NRKFilm:
                     self.log.start('[CACHED] Loading ' + element + ' from cache', indent=1)
 
                     if films[element]:
-                        self.log.success('Loaded "' + films[element]['nrk']['title'] + '"')
+                        self.log.success('Loaded "' + films[element]['nrk']['title'] + '" as "' + films[element]['tmdb']['title'] + '"')
                     else:
                         self.log.warning('Skipped')
 
@@ -337,7 +337,7 @@ class NRKFilm:
 
                         # Add element to cache
                         films[element] = film
-                        
+
         # Return
         return films
 
